922c6732e48270215af514c83688ad68d3bca65d
elationbase <elationbase@gmail.com>
2015-05-19 10:06:14 -0400

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gulpUtil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 var coffeeScr = 'components/coffee/',
     coffeeFiles = [coffeeScr+ '*.coffee'];
@@ -19,5 +20,6 @@ var jsScr = 'components/scripts/',
 gulp.task('js', function(){
   gulp.src(jsFiles)
       .pipe(concat('script.js'))
+      .pipe(browserify())
       .pipe(gulp.dest('builts/development/js'))
 });
